175a1c6a45e854f2e9b470732ffad534c56c21e6
Shukruth-Gowda <gshukruth@gmail.com>
2026-04-13 08:45:31 +0530

    Update App.java

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -11,8 +11,9 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class App 
 {
     public static void main( String[] args )
-    {
-        WebDriver driver=new ChromeDriver();
+    {   
+        options.addArguments("--headless");
+        WebDriver driver=new ChromeDriver(options);
         driver.get("https://www.saucedemo.com/");
         driver.manage().window().maximize();
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
